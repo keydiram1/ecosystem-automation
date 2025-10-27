46dbfc50b96557f08bcce941386c2b7b8401fe0c
Ed Ram <edi3210@gmail.com>
2025-10-27 12:13:57 +0200

    refactoring

--- petstore/src/main/java/api/petstore/RestClient.java
+++ petstore/src/main/java/api/petstore/RestClient.java
@@ -1,5 +1,6 @@
 package api.petstore;
 
+import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -23,7 +24,7 @@ public final class RestClient {
         return get(path, null, 200, type);
     }
     public <T> T get(String path, Map<String, ?> query, int expected, Class<T> type) {
-        var req = io.restassured.RestAssured.given().spec(baseSpec).log().all();
+        var req = RestAssured.given().spec(baseSpec).log().all();
         if (query != null && !query.isEmpty()) req = req.queryParams(query);
         var resp = req.when().get(path);
         return resp.then().statusCode(expected).log().all().extract().as(type);
@@ -33,7 +34,7 @@ public final class RestClient {
         return post(path, body, 200, type);
     }
     public <T> T post(String path, Object body, int expected, Class<T> type) {
-        var resp = io.restassured.RestAssured.given().spec(baseSpec).log().all()
+        var resp = RestAssured.given().spec(baseSpec).log().all()
                 .body(body).when().post(path);
         return resp.then().statusCode(expected).log().all().extract().as(type);
     }
@@ -42,19 +43,19 @@ public final class RestClient {
         return put(path, body, 200, type);
     }
     public <T> T put(String path, Object body, int expected, Class<T> type) {
-        var resp = io.restassured.RestAssured.given().spec(baseSpec).log().all()
+        var resp = RestAssured.given().spec(baseSpec).log().all()
                 .body(body).when().put(path);
         return resp.then().statusCode(expected).log().all().extract().as(type);
     }
 
     // ---- Raw + helpers ----
     public Response getRaw(String path) {
-        var resp = io.restassured.RestAssured.given().spec(baseSpec).log().all().when().get(path);
+        var resp = RestAssured.given().spec(baseSpec).log().all().when().get(path);
         return resp.then().log().all().extract().response();
     }
 
     public Response deleteRaw(String path) {
-        var resp = io.restassured.RestAssured.given().spec(baseSpec).log().all().when().delete(path);
+        var resp = RestAssured.given().spec(baseSpec).log().all().when().delete(path);
         return resp.then().log().all().extract().response();
     }
 
